a534ef165a108e9f285ef3827bf64fe05da03a5a
jesusgr <jesusgr9511@gmail.com>
2025-04-10 16:00:53 -0600

    Se Muestra la informacion almacenada en la pagina cuando se abra

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let tablaListaCompras = document.getElementById("tablaListaCompras");
 const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
 
 const contadorProductos = document.getElementById("contadorProductos")
-const productosTotal = document.getElementById("ProductosTotal")
+const productosTotal = document.getElementById("productosTotal")/////////
 const precioTotal = document.getElementById("precioTotal")
 
 //Numeracion de la primera columna de la tabla
@@ -15,6 +15,7 @@ let cont = 0;
 let costoTotal = 0;
 let totalEnProductos =0;
 let datos = new Array (); //Almacena los elementos
+
 function validarCantidad(){
     if(txtNumber.value.trim().length<=0){
         return false;
@@ -71,20 +72,58 @@ btnAgregar.addEventListener("click", function(event){
                      <td>${txtNumber.value}</td>
                      <td>${precio}</td>
                     </tr>`;
+        let elemento = {
+                         "cont" : cont,
+                         "nombre" : txtName.value,
+                         "cantidad" : txtNumber.value,
+                         "precio" : precio
+                        };
+        datos.push(elemento);
+
+        localStorage.setItem("datos", JSON.stringify(datos));
+ 
         cuerpoTabla.insertAdjacentHTML("beforeend", row);
         costoTotal += precio * Number(txtNumber.value);
         precioTotal.innerText = "$ " + costoTotal.toFixed(2);
         totalEnProductos += Number(txtNumber.value);
         productosTotal.innerText = totalEnProductos;
         contadorProductos.innerText = cont;
-
-
+        let resumen = {
+            "cont" : cont,
+            "totalEnProductos" : totalEnProductos,
+            "costoTotal" : costoTotal
+          };
+        localStorage.setItem("resuemen", JSON.stringify(resumen));
         txtName.value = "";
         txtNumber.value = "";
         txtName.focus();
     }//if isValid
+}); //btnAgregar.addEventListener click
 
+window.addEventListener("load", function(event){
+    event.preventDefault();
+    if(this.localStorage.getItem("datos")!=null){
+        datos = JSON.parse(this.localStorage.getItem("datos"));
+    }// datos !=null
+    datos.forEach((d) => {
+        let row = `<tr>
+                   <td>${d.cont}</td>
+                   <td>${d.nombre}</td>
+                   <td>${d.cantidad}</td>
+                   <td>${d.precio}</td>
+                   </tr>`;
+        cuerpoTabla.insertAdjacentHTML("beforeend", row);
+    });
 
-}); //btnAgregar
+    if(this.localStorage.getItem("resumen")!=null){
+        let resumen = JSON.parse(this.localStorage.getItem("resumen"));
+        costoTotal = resumen.costoTotal;
+        totalEnProductos = resumen.totalEnProductos;
+        cont = resumen.cont;
+    }//// resumen !=null
+    precioTotal.innerText = "$" + costoTotal.toFixed(2);
+    productosTotal.innerText =totalEnProductos;
+    contadorProductos.innerText = cont; 
+});//window.addEventListener load
 
 
